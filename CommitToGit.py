c85ad674d8345a80aa9990b8ed482729840a7199
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-01-13 13:16:02 +0000

    Sunday 13 January 2019
    Sent with love from Python!

---
+++ CommitToGit.py
@@ -0,0 +1,31 @@
+import subprocess, time, datetime
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+keyboard.press(Key.cmd)
+keyboard.release(Key.cmd)
+
+time.sleep(2)
+keyboard.type("cmd")
+
+time.sleep(2)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+time.sleep(2)
+keyboard.type(r"cd C:\Users\ryan_\Desktop\DailyWeighIn\DailyWeighIn")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+time.sleep(2)
+keyboard.type("git status")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+
+#Commit to Git
+time.sleep(2)
+today = datetime.datetime.now()
+keyboard.type('git commit -m "')
+keyboard.type(today.strftime('%A %d %B %Y"'))
+keyboard.type(' -m "Sent with love from Python!"')
